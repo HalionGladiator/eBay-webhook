9bdaa7b83df94e7659aba28ae380693668c1bb0a
HalionGladiator <haliongladiator@gmail.com>
2025-04-15 17:07:24 -0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+
 app.use(express.json());
 
+// Your verification token and full endpoint URL
 const VERIFICATION_TOKEN = 'ZtYxWvUtSrQpOnMlKjHgFdSaReWqXyTuv';
-const ENDPOINT_URL = 'https://ebay-webhook-gipf.onrender.com/' //  update after deploy
+const ENDPOINT_URL = 'https://ebay-webhook-gipf.onrender.com/notify';
 
+// Handle eBay's verification request
 app.get('/notify', (req, res) => {
   const challengeCode = req.query.challenge_code;
+
   if (!challengeCode) {
     return res.status(400).send('Missing challenge_code');
   }
@@ -19,6 +23,7 @@ app.get('/notify', (req, res) => {
   res.json({ challengeResponse });
 });
 
+// Handle account deletion notifications
 app.post('/notify', (req, res) => {
   console.log('Received account deletion notification:');
   console.log(req.body);
@@ -29,11 +34,12 @@ app.post('/notify', (req, res) => {
         <Ack>Success</Ack>
       </soapenv:Body>
     </soapenv:Envelope>`;
-    
+
   res.set('Content-Type', 'application/xml');
   res.send(responseXML);
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
